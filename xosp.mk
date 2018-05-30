6f543d521f8dedb1a4498bb34e2599dce9b4baf8
Gumball1506 <38982972+Gumball1506@users.noreply.github.com>
2018-05-30 18:13:28 +0700

    Update xosp.mk

--- xosp.mk
+++ xosp.mk
@@ -1,5 +1,5 @@
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/fortunave3g/full_fortunave3g.mk)
