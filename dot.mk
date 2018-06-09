34b284b40fe5a8f7100e3a424376f2f7e8171999
Gumball1506 <38982972+Gumball1506@users.noreply.github.com>
2018-06-09 20:11:47 +0700

    Update and rename lineage.mk to dot.mk

--- dot.mk
+++ dot.mk
@@ -1,9 +1,9 @@
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/fortunave3g/full_fortunave3g.mk)
 
 # Setup device configuration
 PRODUCT_DEVICE := fortunave3g
-PRODUCT_NAME := lineage_fortunave3g
+PRODUCT_NAME := dot_fortunave3g
